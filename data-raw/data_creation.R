f58e32354ec5993c7a6f7d4852dfd1319fd17339
Marc Choisy <mchoisy@gmail.com>
2017-04-17 16:43:52 +0700

    Saving data

--- data-raw/data_creation.R
+++ data-raw/data_creation.R
@@ -8,3 +8,6 @@ communes_r <- readOGR("data-raw", "communes")
 provinces <- thinnedSpatialPoly(provinces_r, .01)
 districts <- thinnedSpatialPoly(districts_r, .0036)
 communes <- thinnedSpatialPoly(communes_r, .00145)
+
+devtools::use_data(provinces, districts, communes, provinces_r, districts_r,
+                   communes_r, overwrite = TRUE)
