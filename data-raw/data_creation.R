dcc7b80633175c63b84c0e542217a00ce053db8c
Marc Choisy <mchoisy@gmail.com>
2017-05-05 08:54:07 +0700

    Adding a "province" column to the shapefiles
    This column is consistent with the other province
    column of the other shape files in the other packages

--- data-raw/data_creation.R
+++ data-raw/data_creation.R
@@ -5,6 +5,21 @@ provinces_r <- readOGR("data-raw", "provinces")
 districts_r <- readOGR("data-raw", "districts")
 communes_r <- readOGR("data-raw", "communes")
 
+pr <- sub("Ba Ria-Vung Tau", "Ba Ria - Vung Tau", provinces_r@data$NAME_ENG)
+provinces_r@data$province <- sub("Thua Thien Hue", "Thua Thien - Hue", pr)
+
+pr <- sub("Ba Ria Vung Tau", "Ba Ria - Vung Tau", districts_r@data$name_prov)
+pr <- sub("Can Tho city", "Can Tho", pr)
+pr <- sub("Da Nang city", "Da Nang", pr)
+pr <- sub("Hai Phong city", "Hai Phong", pr)
+pr <- sub("Ho Chi Minh city", "Ho Chi Minh", pr)
+pr <- sub("Quang BInh", "Quang Binh", pr)
+districts_r@data$province <- sub("Thua Thien Hue", "Thua Thien - Hue", pr)
+
+pr <- sub("Thua Thien Hue", "Thua Thien - Hue", communes_r@data$PRO_NAME_E)
+pr <- sub("Ninh Bình", "Ninh Binh", pr)
+communes_r@data$province <- sub("Ba Ria Vung Tau", "Ba Ria - Vung Tau", pr)
+
 provinces <- thinnedSpatialPoly(provinces_r, .01)
 districts <- thinnedSpatialPoly(districts_r, .0036)
 communes <- thinnedSpatialPoly(communes_r, .00145)
