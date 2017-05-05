6222e23ea8d9bf36885f46d11892e518b8f9f799
Marc Choisy <mchoisy@gmail.com>
2017-05-05 14:48:52 +0700

    Adding the package description.

---
+++ R/vietnam63.R
@@ -0,0 +1,7 @@
+#' vietnam63: Shape files of the communes, districts and provinces of Vietnam as of after 2008
+#'
+#' The vietnam63 package provides the polygons of the communes, districts and provinces of Vietnam as defined since 2008.
+#'
+#' @docType package
+#' @name vietnam63
+NULL
